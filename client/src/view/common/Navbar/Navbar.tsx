18822f71b94d4682258f64fbb6621dce7b7606af
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-03 16:34:37 +0530

    Updated Navbar.tsx

--- client/src/view/common/Navbar/Navbar.tsx
+++ client/src/view/common/Navbar/Navbar.tsx
@@ -18,23 +18,23 @@ export class Navbar extends Component {
 
                 <Link to="/">
                     <div className="flex">
-                        <img className="h-16 ml-6 pt-1" src={logo}
+                        <img className="h-16 ml-6 pt-1 hover:scale-110" src={logo}
                              alt=""/>
                         <h1 className="ml-2 mt-5 uppercase text-[18px] text-[#0af] font-bold">Techno computers</h1>
                     </div>
                 </Link>
 
                 <ul className="list-none flex mt-1">
-                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc]">
+                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc] hover:scale-110">
                         <Link to="/"><FontAwesomeIcon icon={faHome}/> Home</Link>
                     </li>
-                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc]">
+                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc] hover:scale-110">
                         <Link to="/about"><FontAwesomeIcon icon={faInfoCircle}/> About Us</Link>
                     </li>
-                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc]">
+                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc] hover:scale-110">
                         <Link to="/contact"><FontAwesomeIcon icon={faContactBook}/> Contact Us</Link>
                     </li>
-                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc]">
+                    <li className="mr-8 text-[16px] mt-3 text-[#e6f0e6] hover:text-[#2cc1fc] hover:scale-110">
                         <Link to="/shoppingCart"><FontAwesomeIcon icon={faShoppingCart}/> My Cart</Link>
                     </li>
                 </ul>
@@ -42,12 +42,12 @@ export class Navbar extends Component {
                 <div>
                     <button className="text-[14px] font-bold text-[#e6f0e6]
                                    bg-[#2cc1fc] pl-3 pr-3 h-10 mt-3
-                                   hover:text-tertiary rounded mr-5">
+                                   hover:text-tertiary rounded mr-5 hover:scale-110">
                         <Link to="/login"><FontAwesomeIcon icon={faSignInAlt}/> Sign In</Link>
                     </button>
                     <button className="text-[14px] font-bold text-[#e6f0e6]
                                    bg-[#36C64C] pl-3 pr-3 h-10 mt-3
-                                   hover:text-tertiary rounded">
+                                   hover:text-tertiary rounded hover:scale-110">
                         <Link to="/signup"><FontAwesomeIcon icon={faUserPlus}/> Sign Up</Link>
                     </button>
                 </div>
