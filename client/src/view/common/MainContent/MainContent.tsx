f262a661c699f332c0630b3544b059c3064180e7
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-03 15:45:08 +0530

    Updated ShoppingCart.tsx & MainContext.tsx

--- client/src/view/common/MainContent/MainContent.tsx
+++ client/src/view/common/MainContent/MainContent.tsx
@@ -4,6 +4,8 @@ import {Contact} from "../../pages/Contact/Contact";
 import {Home} from "../../pages/Home/Home";
 import {Route, Routes} from "react-router-dom";
 import {ShoppingCart} from "../../pages/ShoppingCart/ShoppingCart";
+import {ModifyCart} from "../ModifyCart/ModifyCart";
+
 export class MainContent extends Component {
     render() {
         return (
@@ -12,7 +14,7 @@ export class MainContent extends Component {
                     <Route path="/" Component={Home}></Route>
                     <Route path="/about" Component={About}></Route>
                     <Route path="/contact" Component={Contact}></Route>
-                    <Route path="/shoppingCart" Component={ShoppingCart}></Route>
+                    <Route path="/shoppingCart" element={<ShoppingCart itemList={ModifyCart.itemsList} />}></Route>
                 </Routes>
             </div>
         );
--- client/src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ client/src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,9 +1,56 @@
 import {Component} from "react";
+import {CartItem} from "../../../model/CartItem";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faX} from '@fortawesome/free-solid-svg-icons'
 
-export class ShoppingCart extends Component {
+interface ShoppingCartProps {
+    itemList: CartItem[];
+}
+
+export class ShoppingCart extends Component<ShoppingCartProps> {
     render() {
         return (
-            <h1>Shopping Cart</h1>
+            <div className="flex justify-center items-start pt-10 pb-10 bg-[url(images/main_bg.jpg)] h-auto">
+                <div className="bg-[#232323] w-full mx-16 justify-center">
+                    <table className="w-full border border-gray-500">
+                        <thead className="h-20 border border-gray-500">
+                        <tr className="text-white text-center">
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Id</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Product Name
+                            </th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Unit Price</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Qty</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Action</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Total Price</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.props.itemList.length === 0 ?
+                                <tr>
+                                    <td colSpan={6} className="h-36 px-1">
+                                        <p className="text-center text-[30px] text-white font-bold">You Have No Items In
+                                            Your Shopping
+                                            Cart.</p>
+                                    </td>
+                                </tr>
+                                : this.props.itemList.map((item) =>
+                                    <tr className="text-white text-center px-1 h-20 border border-gray-500">
+                                        <td className="px-1 border border-gray-500">{item.product.id}</td>
+                                        <td className="px-1 border border-gray-500">{item.product.description}</td>
+                                        <td className="px-1 border border-gray-500">{item.product.price + ' ' + item.product.currency}</td>
+                                        <td className="px-1 border border-gray-500">{item.itemCount}</td>
+                                        <td className="px-1 hover:cursor-pointer hover:text-red-600 border border-gray-500">
+                                            <FontAwesomeIcon
+                                                icon={faX}/></td>
+                                        <td className="px-1 border border-gray-500">{(item.product.price * item.itemCount) + ' ' + item.product.currency}</td>
+                                    </tr>
+                                )
+                        }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 }
