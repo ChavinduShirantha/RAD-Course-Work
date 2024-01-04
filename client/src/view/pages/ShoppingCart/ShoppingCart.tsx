7eb4fefae4aa43f213b4dc0a4f0596669e73ce36
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-04 11:47:04 +0530

    Updated ShoppingCart.tsx

--- client/src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ client/src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -11,8 +11,8 @@ export class ShoppingCart extends Component<ShoppingCartProps> {
     render() {
         return (
             <div className="flex justify-center items-start pt-10 pb-10 bg-[url(images/main_bg.jpg)] h-auto">
-                <div className="bg-[#232323] w-full mx-16 justify-center">
-                    <table className="w-full border border-gray-500">
+                <div className="bg-[#232323] w-full mx-16 justify-center pr-10 pl-10">
+                    <table className="w-full border mt-10 border-gray-500">
                         <thead className="h-20 border border-gray-500">
                         <tr className="text-white text-center">
                             <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Id</th>
@@ -49,6 +49,7 @@ export class ShoppingCart extends Component<ShoppingCartProps> {
                         }
                         </tbody>
                     </table>
+                    <button className="float-right mt-10 mb-10 pl-6 pr-6 pt-2 pb-2 bg-[#2cc1fc] text-[16px] font-bold text-white rounded uppercase border-[2px] border-[#2cc1fc] hover:bg-[#444544] hover:text-[#2cc1fc] hover:border-[2px] hover:border-[#2cc1fc]">Purchase</button>
                 </div>
             </div>
         );
