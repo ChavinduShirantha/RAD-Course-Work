617c3044b34c1601996f2a27cc352f9719967aff
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-09 15:21:38 +0530

    Added constant.js

---
+++ server/constants/constants.js
@@ -0,0 +1,49 @@
+const productsList = [
+    {
+        "id": 1,
+        "description": "Intel Core I3-8GB-128GB+1TB 10th Gen PC",
+        "name": "- Brand new desktop -",
+        "price": 122200,
+        "currency": ".00 LKR",
+        "image": "product1.jpg",
+        "productState": "in_stock.png"
+    },
+    {
+        "id": 2,
+        "description": "Havit Gamenote H2026d Gaming Headset",
+        "name": "- Speaker & Headphone -",
+        "price": 4500,
+        "currency": ".00 LKR",
+        "image": "product2.jpg",
+        "productState": "new_arrivals.png"
+    },
+    {
+        "id": 3,
+        "description": "Havit KB500L RGB Backlit Gaming Keyboard",
+        "name": "- Keyboard & Mouse -",
+        "price": 3800,
+        "currency": ".00 LKR",
+        "image": "product3.jpg",
+        "productState": "in_stock.png"
+    },
+    {
+        "id": 4,
+        "description": "Havit Gamenote MS1027 Gaming Mouse",
+        "name": "- Keyboard & Mouse -",
+        "price": 1400,
+        "currency": ".00 LKR",
+        "image": "product4.jpg",
+        "productState": "new_arrivals.png"
+    },
+    {
+        "id": 5,
+        "description": "Kingston DataTraveler Exodia 64GB USB Flash Drive",
+        "name": "- Pen Drive & SD Card -",
+        "price": 1600.00,
+        "currency": ".00 LKR",
+        "image": "product5.jpg",
+        "productState": "out_of_stock.png"
+    }
+]
+
+module.exports = productsList;
