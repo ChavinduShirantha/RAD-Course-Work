c1257e6d0d16d36587aaef119092ec504cb0a14b
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-10 17:06:52 +0530

    Updated AdminSideBar.tsx & AdminMainContent.tsx

--- client/src/view/adminPages/AdminMainContent/AdminMainContent.tsx
+++ client/src/view/adminPages/AdminMainContent/AdminMainContent.tsx
@@ -7,10 +7,10 @@ export class AdminMainContent extends Component {
     render() {
         return (
             <div className="flex flex-row mb-0">
-                <div className="flex basis-1/6 bg-green-200 ">
+                <div className="flex basis-1/6 bg-[#444544] ">
                     <AdminSideBar/>
                 </div>
-                <div className="flex basis-5/6 bg-green-700">
+                <div className="flex basis-5/6 ">
                     <Routes>
                         <Route path="/" Component={AdminHome}></Route>
                     </Routes>
--- client/src/view/adminPages/AdminSideBar/AdminSideBar.tsx
+++ client/src/view/adminPages/AdminSideBar/AdminSideBar.tsx
@@ -3,8 +3,10 @@ import {Component} from "react";
 export class AdminSideBar extends Component {
     render() {
         return (
-            <div>
-                <h1>SideBar</h1>
+            <div className="relative mt-10 mb-10 mx-auto">
+                <button className="w-full px-2 bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Dashboard</button>
+                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Customers</button>
+                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Products</button>
             </div>
         );
     }
