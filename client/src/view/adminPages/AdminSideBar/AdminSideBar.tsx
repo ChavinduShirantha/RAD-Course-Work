50e2239150f6f5811bff2016ef64548605eafa8f
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-10 17:58:23 +0530

    Updated AdminSideBar.tsx

--- client/src/view/adminPages/AdminSideBar/AdminSideBar.tsx
+++ client/src/view/adminPages/AdminSideBar/AdminSideBar.tsx
@@ -1,12 +1,20 @@
 import {Component} from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBorderAll, faUsers, faComputer} from "@fortawesome/free-solid-svg-icons";
 
 export class AdminSideBar extends Component {
     render() {
         return (
             <div className="relative mt-10 mb-10 mx-auto">
-                <button className="w-full px-2 bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Dashboard</button>
-                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Customers</button>
-                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white font-bold hover:scale-105">Products</button>
+                <button className="w-full px-2 bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white
+                                font-bold hover:scale-105"><FontAwesomeIcon icon={faBorderAll}/> Dashboard
+                </button>
+                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white
+                                font-bold hover:scale-105"><FontAwesomeIcon icon={faUsers}/> Customers
+                </button>
+                <button className="w-full bg-[#2cc1fc] mb-5 h-16 rounded-2xl text-white
+                                font-bold hover:scale-105"><FontAwesomeIcon icon={faComputer}/> Products
+                </button>
             </div>
         );
     }
