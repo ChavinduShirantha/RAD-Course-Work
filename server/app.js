b95519ba0db823c580c3db8b94963fdb4497fc11
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-13 10:02:32 +0530

    Added DBConnection.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const DBConnection = require('./db/DBConnection');
+
+DBConnection.then(r => console.log(r));
 
 var indexRouter = require('./routes/index');
 var contactRouter = require('./routes/contact');
